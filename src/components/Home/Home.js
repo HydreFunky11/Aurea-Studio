86b46031a6167855d4b1c31d960a83b1c5811877
remy <remy.berjonneau@gmail.com>
2026-03-18 10:13:51 +0100

    Fix #2: Adjust title layout and size

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,7 +28,10 @@ export default function Home() {
           <span className={styles.taglineLine}></span>
         </div>
 
-        <h1 className={styles.title}>Aurea Event</h1>
+        <h1 className={styles.title}>
+          <span className={styles.titleAurea}>Aurea</span>
+          <span className={styles.titleEvent}>Event</span>
+        </h1>
 
         <h2 className={styles.subtitle}>
           Révélateur de <span className={styles.goldText}>Talents</span>
