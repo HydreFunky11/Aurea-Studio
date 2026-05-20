d5cc1b2a133da21b0c5b8b37328c9bf343515e13
remy <remy.berjonneau@gmail.com>
2026-05-20 12:18:32 +0200

    fix dark theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
